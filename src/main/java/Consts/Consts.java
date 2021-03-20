d1613962347cb136a8ca167090267ded86229760
Eduard Vaiser <vellee@ukr.net>
2021-03-19 22:34:08 -0500

    Addition of BasePage, Consts, SharedDriver, UseCaseBase classes

---
+++ src/main/java/Consts/Consts.java
@@ -0,0 +1,15 @@
+package Consts;
+
+public class Consts {
+    public static final String MAIN_URL = "https://www.accesstravel.com/en-US   ";
+    public static final String DESTINATIONS_LIST_URL = "https://www.accesstravel.com/en-US/Destination/list";
+    public static final String TRAVEL_BLOGS_URL = "https://www.accesstravel.com/en-US/Story/List";
+    public static final String TRAVEL_FORUM_URL = "https://www.accesstravel.com/en-US/Forum/List";
+    public static final String HOTEL_LIST_URL = "https://www.accesstravel.com/en-US/Hotel/List";
+    public static final String REGISTRATION_PAGE_URL = "https://www.accesstravel.com/en-US/Account/Register";
+    public static final String ABOUT_US_URL = "https://www.accesstravel.com/en-US/Home/AboutUs";
+    public static final String CUSTOMER_AGREEMENT_URL = "https://www.accesstravel.com/en-US/Home/CustomerAgreement";
+    public static final String PRIVACY_POLICY_URL = "https://www.accesstravel.com/en-US/Home/PrivacyPolicy";
+    public static final String HELP_CENTER_URL = "https://www.accesstravel.com/en-US/Home/HelpCenter";
+
+}
